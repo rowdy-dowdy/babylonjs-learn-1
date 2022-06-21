d7ac97d521a1fb2827641fa5b39b9a41b9bb1e98
Việt Hùng <viet.hung.2898@gmail.com>
2022-06-21 04:13:31 +0000

    add

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+  import { createScene } from "./sence";
+  let el: HTMLElement;
+  onMount(() => {
+    createScene(el)
+  });
+</script>
+
+<svelte:head>
+	<title>Babylonjs Learn 1</title>
+	<meta name="description" content="Babylonjs Learn 1" />
+</svelte:head>
+
+<canvas bind:this={el}></canvas>
+
+<style>
+	canvas {
+		width: 100%;
+		height: 100%;
+		outline: none;
+	}
+</style>
